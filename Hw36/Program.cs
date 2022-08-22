a3942a61ca940962dd69478ddbfbbb55e646bce0
Dinis <Dinis_abd@example.com>
2022-08-22 20:01:32 +0300

    Begin

--- Hw36/Program.cs
+++ Hw36/Program.cs
@@ -3,3 +3,47 @@
 // Пример:
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]},");
+        else Console.Write(array[i]);
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+int[] GetSumOddInd(int[] array)
+{
+    int sumNeg = 0;
+    int sumPos = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < 0) sumNeg += array[i];
+        else sumPos += array[i];
+    }
+
+    return new int[] { sumNeg, sumPos };
+}
+
+int[] arr = CreateArrayRndInt(12, -9, 9);
+PrintArray(arr);
+int[] sumOddInd = int[] GetSumOddInd(arr);
+Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях (индексах) -> {sumOddInd[0]}");
