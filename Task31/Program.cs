969c7b7d4edb74e829eee0c80685142a7151e0e4
Dinis <Dinis_abd@example.com>
2022-08-22 18:17:33 +0300

    Begin

--- Task31/Program.cs
+++ Task31/Program.cs
@@ -22,14 +22,31 @@ int[] CreateArrayRndInt(int size, int min, int max)
 void PrintArray(int[] array)
 {
     Console.Write("[");
-    for (int i = 0; i < array.Lenght; i++)
+    for (int i = 0; i < array.Length; i++)
     {
         if (i < array.Length - 1) Console.Write($"{array[i]},");
-        else Console.Write($"{array[i]}");
+        else Console.Write(array[i]);
     }
     Console.Write("]");
-    Console.WriteLine;
+    Console.WriteLine();
 }
 
-int[] arr = CreateArrayRndInt(12, -9, 90);
-PrintArray();
+int[] GetSumPosNegElem(int[] array)
+{
+    int sumNeg = 0;
+    int sumPos = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < 0) sumNeg += array[i];
+        else sumPos += array[i];
+    }
+
+    return new int[] { sumNeg, sumPos };
+}
+
+int[] arr = CreateArrayRndInt(12, -9, 9);
+PrintArray(arr);
+int[] sumPosNegElem = GetSumPosNegElem(arr);
+Console.WriteLine($"Сумма отрицательных чисел = {sumPosNegElem[0]}");
+Console.WriteLine($"Сумма положительных чисел = {sumPosNegElem[1]}");
